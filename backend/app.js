a3269e30165d58db177eac953c1e16b040203ff4
Marwan <marwanpsh@gmail.com>
2025-06-29 23:54:18 +0500

    Fixed Error

--- backend/app.js
+++ backend/app.js
@@ -7,30 +7,17 @@ import mongoose from "mongoose";
 import companyRoutes from "./routes/companies.js";
 import quotationRoutes from "./routes/quotations.js";
 
+const app = express();
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors({ origin: "*" }));
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.send("Backend is working!");
-});
 
 app.use("/api/companies", companyRoutes);
 app.use("/api/quotations", quotationRoutes);
 
-// Connect to DB and then start server
-mongoose
-  .connect(process.env.DB_URI)
-  .then(() => {
-    console.log("Connected to db");
-    app.listen(port, () => console.log(`Listening on port ${port}`));
-  })
-  .catch((err) => {
-    console.error("Failed to connect to DB", err);
-  });
+app.listen(port, () => console.log(`Listening on port ${port}`));
+mongoose.connect(process.env.DB_URI, () => console.log("Connected to db"));
